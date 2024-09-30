3258965144e7d8cdd2facfb59e7b263c07a73347
neemzy <tom.panier@free.fr>
2024-09-30 11:20:01 +0200

    Add posts table and migration

---
+++ database/migrations/1727687962547_create_posts_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('title').notNullable()
+      table.string('content').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
